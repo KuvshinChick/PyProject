ac837f05d3a1ae90e811322f12a3bd1209eb5f2d
KuvshinChick <kuvshinirina75@gmail.com>
2022-10-15 04:17:18 +0300

    +PyProgs

---
+++ PyCharm/arithmetic.py
@@ -0,0 +1,6 @@
+print ("Дайте ответ на пример: 4 * 100 - 54 ")
+user_ans = input("Ваш ответ: ")
+right_ans = 4*100-54
+# print ("\n","Your answer was:",user_ans,"\n","Right answer is",Right_ans)
+print ("Your answer was:",user_ans)
+print ("Right answer is:",right_ans)
---
+++ PyCharm/user.py
@@ -0,0 +1,5 @@
+Name = input("What is your name?")
+Age = input("How old are you?")
+Place_Of_Living = input("Where do you live?")
+
+print ("\n","This is ",Name,"\n","It is ",Age,"\n","(S)he live in ",Place_Of_Living)
